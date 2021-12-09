3dd67ea2380d8a8642fc3a32cea6662a9fc66151
Eric Esteban <eric_7@hotmail.com>
2021-12-09 20:57:40 +0100

    HomePageLogedin

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,21 +1,38 @@
 import RandomJoke from "../components/RandomJoke";
 import Logo from "../Images/LOGO1.png";
+import { AuthContext } from "./../context/auth.context";
+import { useContext } from "react";
 
 function HomePage() {
+  const { isLoggedIn } = useContext(AuthContext);
   return (
     <div>
       <img style={{ width: 350 }} src={Logo} alt="noky-toky logo" />
-      <h2>filter cathegory component*</h2>
-      <RandomJoke />
-      <div className="divIconHomePage">
-        <a href="#" className="previous">
-          &hearts; Like
-        </a>
-        <a href="#" className="next">
-          Next &raquo;
-        </a>
-      </div>
-      <h2>Buy merch</h2>
+      {isLoggedIn ? (
+        <>
+          <h2>filter cathegory component*</h2>
+          <RandomJoke />
+          <div className="divIconHomePage">
+            <a href="#" className="previous">
+              &hearts; Like
+            </a>
+            <a href="#" className="next">
+              Next &raquo;
+            </a>
+          </div>
+          <h2>Buy merch</h2>
+        </>
+      ) : (
+        <>
+          <RandomJoke />
+          <div className="divIconHomePage">
+            <a href="#" className="next">
+              Next &raquo;
+            </a>
+          </div>
+          <h2>Buy merch</h2>
+        </>
+      )}
     </div>
   );
 }
